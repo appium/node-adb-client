30349236ca514f18ce69e755ae93838068e9187a
Callum Styan <callumstyan@gmail.com>
2015-08-06 10:43:24 -0700

    fix index and adb-devices to handle new multi device support

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 import ADBDevice from './lib/adb-device';
 
-function connectToDevice (type) {
-  return new ADBDevice(type);
+function connectToDevice (type, device) {
+  return new ADBDevice(type, device);
 }
 
 export { connectToDevice };
