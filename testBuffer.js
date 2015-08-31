9b107bb7ab37be94c4e614757b519a55e2b647c4
Callum Styan <callumstyan@gmail.com>
2015-08-31 11:09:20 -0700

    remove accidental commit of a temp file

--- testBuffer.js
+++
@@ -1,9 +0,0 @@
-let testBuffer = new Buffer(4);
-testBuffer.writeUInt32LE(65536);
-console.log(testBuffer);
-let secondBuffer =  new Buffer("test");
-testBuffer = Buffer.concat([testBuffer, secondBuffer]);
-console.log("testBuffer: ", testBuffer);
-testBuffer = new Buffer("");
-console.log("testBuffer: ", testBuffer);
-console.log("testBuffer length:", testBuffer.length);
